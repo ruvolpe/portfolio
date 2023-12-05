c3dfdfa818f9614f785eab3e182fcb1f5d60ebbe
ruvolpe <ru.volpe@gmail.com>
2023-12-05 11:14:13 -0300

    feat: initialized project

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { Header } from "./components/Header";
+import { Banner } from "./components/Banner";
+import { Techs } from "./components/Techs";
+import { Social } from "./components/Social";
+import { Footer } from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Banner />
+      <Techs />
+      <Social />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Techs/index.jsx
@@ -0,0 +1,5 @@
+function Techs() {
+  return <></>;
+}
+
+export default Techs;
